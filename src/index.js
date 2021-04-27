bc65c644e7445011b92dcbba430c10baf93b5e5d
AurobindoGupta <aurogpt10@gmail.com>
2021-04-28 00:06:47 +0530

    start evernote tutorial;

--- src/index.js
+++ src/index.js
@@ -3,12 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase';
+
+
+// const firebase = require('firebase');
+// require('firebase/firestore');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBURyDBhPlPe6dk_rCTJdtkw6KuogNSDLw",
+  authDomain: "evernote-clone-299b0.firebaseapp.com",
+  projectId: "evernote-clone-299b0",
+  storageBucket: "evernote-clone-299b0.appspot.com",
+  messagingSenderId: "777239017618",
+  appId: "1:777239017618:web:ac1b014d021aeba2828e4b",
+  measurementId: "G-SPYSG8HHFS"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+   <App />,
+  document.getElementById('evernote-container')
 );
 
 // If you want to start measuring performance in your app, pass a function
